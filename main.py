902e473095f7d48a75760f354c1c76211a48dc17
Kush <kokatekush@gmail.com>
2024-03-23 18:58:24 +0530

    adding authentication

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ AGE = 19
 
 APP_ID = "3fb3ac02"
 API_KEY = "f3eb209fd5360ef39328a1ff6398c19a	"
+BEARER_TOKEN = "Bearer BeardCC123"
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 sheety_endpoint = "https://api.sheety.co/4ee973ff7ebca45d7aaab62454d98841/workoutTracking/workouts"
 exercise_output = input("What exercise did you do ? ")
@@ -38,7 +39,9 @@ for exercise in result["exercises"]:
             "calories": exercise["nf_calories"]
         }
     }
+bearer_headers = {
+    "Authorization": BEARER_TOKEN,
+}
 
-    sheet_response = requests.post(url = sheety_endpoint, json=sheet_inputs)
-
-    print(sheet_response.text)
+sheet_response = requests.post(url = sheety_endpoint, json=sheet_inputs, headers = bearer_headers )
+print(sheet_response.text)
