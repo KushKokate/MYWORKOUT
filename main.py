b1759f2ddabae13d29a35476a153638dffe2818b
KUSH KOKATE <142797968+KushKokate@users.noreply.github.com>
2024-03-24 21:39:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ AGE = 19
 
 
 APP_ID = "3fb3ac02"
-API_KEY = "f3eb209fd5360ef39328a1ff6398c19a	"
+API_KEY = "f3eb209fd5360ef39328a1ff6398c19a"
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 sheety_endpoint = "https://api.sheety.co/4ee973ff7ebca45d7aaab62454d98841/workoutTracking/workouts"
 exercise_output = input("What exercise did you do ? ")
